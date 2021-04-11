b0b0968c4135d537b894deb490d187f957048bcd
乐洋 <yueyang130@qq.com>
2021-04-11 23:00:37 +0800

    page 3(section) is ongoing

--- pages/sections/sections-home/sections-home.js
+++ pages/sections/sections-home/sections-home.js
@@ -1,66 +1,101 @@
-// pages/sections/sections-home/sections-home.js
-Page({
 
+Page({
+ 
     /**
      * 页面的初始数据
      */
     data: {
+        winWidth:0,
+        winHeight:0,
+        currentTab:0,
 
+        forum_list: [
+            // TODO： 暂时先随便放个图标，之后需更换为符合主题的图标
+            {
+                id : 0,
+                title : "社团",
+                picUrl : "/icon/sample.png"
+            },
+            {
+                id : 1,
+                title : "博雅",
+                picUrl : "/icon/sample.png"
+            },
+            {
+                id : 2,
+                title : "学生会",
+                picUrl : "/icon/sample.png"
+            },
+            {
+                id : 3,
+                title : "志愿",
+                picUrl : "/icon/sample.png"
+            },
+            {
+                id : 4,
+                title : "个人",
+                picUrl : "/icon/sample.png"
+            },
+
+        ]
     },
-
+ 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        var that = this;
+ 
+        /** 
+         * 获取系统信息 
+         */
+        wx.getSystemInfo({
+ 
+            success: function (res) {
+                that.setData({
+                    winWidth: res.windowWidth,
+                    winHeight: res.windowHeight
+                });
+            }
+ 
+        });  
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
+    bindChange: function (e) {
+ 
+        var that = this;
+        that.setData({ currentTab: e.detail.current });
+ 
     },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
+    swichNav: function (e) {
+ 
+        var that = this;
+ 
+        if (this.data.currentTab === e.target.dataset.current) {
+            return false;
+        } else {
+            that.setData({
+                currentTab: e.target.dataset.current
+            })
+        }
+    } ,  
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
     onPullDownRefresh: function () {
-
+ 
     },
-
+ 
     /**
      * 页面上拉触底事件的处理函数
      */
     onReachBottom: function () {
-
+ 
     },
-
+ 
     /**
      * 用户点击右上角分享
      */
     onShareAppMessage: function () {
-
+ 
     }
 })
