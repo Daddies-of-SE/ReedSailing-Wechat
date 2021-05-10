e324be154ba388fb79ce51ce5a904b598922241f
wzk <18231047@buaa.edu.cn>
2021-05-11 01:03:01 +0800

    update server url

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ App({
     },
 
   ],
-  server : 'https://www.reedsailing.xyz/',
+  server : 'https://www.reedsailing.xyz/api/',
   // server : 'http://rs.test/',
   // server : 'http://127.0.0.1:8000/',
 
