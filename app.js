ae45545842fafaffd06d8de61b876a1cd51a7590
wzk <18231047@buaa.edu.cn>
2021-05-16 21:07:19 +0800

    update readme

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ App({
   ],
   server : 'https://www.reedsailing.xyz/api/',
   ws_werver: 'wss://www.reedsailing.xyz/ws/',
+  // ws_werver: 'wss://rs.test/ws/',
   // server : 'http://rs.test/',
   // server : 'http://127.0.0.1:8000/api/',
   // ws_werver: 'ws://127.0.0.1:8000/ws/',
